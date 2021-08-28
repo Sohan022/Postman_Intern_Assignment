d6492d7a34e6f8f30e1904e866b497d7aa0606f9
Sohan Lal Gupta <sohanlalgupta022@gmail.com>
2021-08-28 19:52:04 +0530

    made private methods & data

--- crawler.py
+++ crawler.py
@@ -2,47 +2,51 @@ import requests
 import time
 from getpass import getpass
 from mysql.connector import connect, Error
+from rich.progress import Progress, BarColumn
 
 class Crawler:
-    categories = []
-    data = []
-    bearerToken = ""
+    __categories = []
+    __data = []
+    __bearerToken = ""
+    __conn = None
+    __cur = None
 
-    try:
-        conn =  connect(
-            host="localhost",
-            user=input("Enter username: "),
-            password=getpass("Enter password: "),
-            database = "crawldata"
-        )
-        cur = conn.cursor()    
-    except Error as e:
-        print(e)
+    def __connectDatabase(self):
+        try:
+            self.__conn =  connect(
+                host="localhost",
+                user=input("Enter username: "),
+                password=getpass("Enter password: "),
+                database = "crawldata"
+            )
+            self.__cur = self.__conn.cursor()    
+        except Error as e:
+            print(e)
     
-    def createTable(self):
+    def __createTable(self):
         drop_table_query = "DROP TABLE IF EXISTS apis"
         create_table_query = """
         CREATE TABLE apis (
             ID INT AUTO_INCREMENT PRIMARY KEY,
-            API VARCHAR(50),
-            Description VARCHAR(200),
+            API VARCHAR(100),
+            Description VARCHAR(500),
             Auth VARCHAR(20),
             HTTPS BOOLEAN,
             Cors VARCHAR(20),
-            Link VARCHAR(100),
-            Category VARCHAR(50)
+            Link VARCHAR(300),
+            Category VARCHAR(100)
         )
         """
         try:
-            self.cur.execute(drop_table_query)
-            self.cur.execute(create_table_query)
-            self.conn.commit()
+            self.__cur.execute(drop_table_query)
+            self.__cur.execute(create_table_query)
+            self.__conn.commit()
         except:
-            self.conn.rollback()
+            self.__conn.rollback()
 
-    def storeDatabase(self):
+    def __storeDatabase(self):
         tupleDataList = []
-        for ele in self.data:
+        for ele in self.__data:
             tple = (ele["API"], ele["Description"], ele["Auth"],ele["HTTPS"],ele["Cors"],ele["Link"],ele["Category"])
             tupleDataList.append(tple)
         
@@ -52,14 +56,15 @@ class Crawler:
             VALUES ( %s, %s, %s, %s, %s, %s, %s )
             """
         try:
-            self.cur.executemany(insert_apis_query,tupleDataList);
-            self.conn.commit()
-        except:
-            self.conn.rollback()
+            self.__cur.executemany(insert_apis_query,tupleDataList);
+            self.__conn.commit()
+        except Error as e:
+            print(e)
+            self.__conn.rollback()
         
 
 
-    def getToken(self):
+    def __getToken(self):
         url = "https://public-apis-api.herokuapp.com/api/v1/auth/token"
         response = requests.request("GET", url)
         if response.status_code == 200:
@@ -69,20 +74,20 @@ class Crawler:
         else:
             return {"error":"unknown error"}
 
-    def setToken(self):
-        token = self.getToken()
+    def __setToken(self):
+        token = self.__getToken()
         if "error" in token:
             if token["error"] == "too many requests":
                 time.sleep(60)
             else:
                 return False
         else:
-            token = self.getToken()
-            self.bearerToken = "".join(("Bearer ",token['token']))
+            token = self.__getToken()
+            self.__bearerToken = "".join(("Bearer ",token['token']))
         return True
         
 
-    def makeRequest(self,url,bearerToken,page = 1,category = None):
+    def __makeRequest(self,url,bearerToken,page = 1,category = None):
         headers = {"Authorization": bearerToken}
         if category != None:
             params = {"page":page, "category":category}
@@ -96,11 +101,11 @@ class Crawler:
         else:
             return {"error":"unknown error"}
     
-    def getAllCategories(self):
+    def __getAllCategories(self):
         page = 1
         while True:
             url = "https://public-apis-api.herokuapp.com/api/v1/apis/categories"
-            categoriesResponse = self.makeRequest(url, self.bearerToken,page)
+            categoriesResponse = self.__makeRequest(url, self.__bearerToken,page)
             if 'error' in categoriesResponse:
                 if categoriesResponse['error'] == "too many requests":
                     time.sleep(60)
@@ -110,46 +115,77 @@ class Crawler:
                 else:
                     return False
             else: 
-                self.categories.extend(categoriesResponse['categories'])
+                self.__categories.extend(categoriesResponse['categories'])
                 if page*10 < categoriesResponse['count']:
                     page += 1
                 else:
                     break
         return True
     
-    def getAllCategoriesAPI(self):
-        for category in self.categories:
-            page = 1
-            while True:
-                url = "https://public-apis-api.herokuapp.com/api/v1/apis/entry"
-                categoriesResponse = self.makeRequest(url, self.bearerToken,page,category)
-                if 'error' in categoriesResponse:
-                    if categoriesResponse['error'] == "too many requests":
-                        time.sleep(60)
-                    elif categoriesResponse['error'] == "Invalid token":
-                        if self.setToken() == False:
+    def __getAllCategoriesAPI(self):
+        progress = Progress("[progress.description]{task.description}",BarColumn(),"[progress.percentage]{task.percentage:>3.0f}%")
+        with progress:
+            task = progress.add_task("[green]Progress...", total=len(self.__categories))
+            for category in self.__categories:
+                page = 1
+                while True:
+                    url = "https://public-apis-api.herokuapp.com/api/v1/apis/entry"
+                    categoriesResponse = self.__makeRequest(url, self.__bearerToken,page,category)
+                    if 'error' in categoriesResponse:
+                        if categoriesResponse['error'] == "too many requests":
+                            time.sleep(60)
+                        elif categoriesResponse['error'] == "Invalid token":
+                            if self.__setToken() == False:
+                                return False
+                        else:
                             return False
                     else:
-                        return False
-                else:
-                    self.data.extend(categoriesResponse['categories'])
-                    if page*10 < categoriesResponse['count']:
-                        page += 1
-                    else:
-                        break
+                        self.__data.extend(categoriesResponse['categories'])
+                        if page*10 < categoriesResponse['count']:
+                            page += 1
+                        else:
+                            break
+                progress.update(task, advance=1)
         return True
 
-#driver code
-crawl = Crawler()
-crawl.createTable()
-if crawl.setToken():
-    categoriesReturn = crawl.getAllCategories()
+
+
+    def crawl(self):
+        if self.__setToken():
+            categoriesReturn = self.__getAllCategories()
+        if categoriesReturn and self.__getAllCategoriesAPI():
+            print("Successfully crawled!")
+        else:
+            print("ERROR: Unknown Error")   
+
+    def saveToDatabase(self):
+        if len(self.__data) == 0:
+            print("No Data Crawled!!!")
+        else:
+            self.__connectDatabase()
+            self.__createTable()
+            self.__storeDatabase()
+            print("successfully Save to Database!")
+
+    def crawlAndSaveToDatabase(self):
+        self.crawl()
+        self.saveToDatabase()
+
+    def printCategories(self):
+        if len(self.__categories) == 0:
+            print("There is not data!!!")
+        else:
+            print(self.__categories)
     
-    if categoriesReturn and crawl.getAllCategoriesAPI():
-        crawl.storeDatabase()
-    else:
-        print("ERROR: Unknown Error")
-        
-else:
-    print("ERROR: Unknown Error")
+    def printAllAPI(self):
+        if len(self.__data) == 0:
+            print("There is not data")
+        else:
+            print(self.__data)
+
+
+
+#driver code
+crawler = Crawler()
+crawler.crawlAndSaveToDatabase()
 
